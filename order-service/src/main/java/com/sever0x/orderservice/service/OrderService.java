55335a48a3f5f68855e5455ce78a885a8cceaf1a
shdwraze <shdwrazedev@gmail.com>
2024-02-10 02:55:32 +0200

    Service discovery via Eureka

--- order-service/src/main/java/com/sever0x/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/sever0x/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     private final OrderMapper orderMapper;
 
@@ -41,8 +41,8 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
